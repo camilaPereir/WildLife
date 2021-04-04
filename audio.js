ffad54d3664453793813f02849a38ddb3c7c6da7
Camila Pereira <camila379.cp@gmail.com>
2021-04-04 20:26:35 -0300

    chess with style modified

---
+++ audio.js
@@ -0,0 +1,17 @@
+
+var sound1 = new Howl({
+    src: ['/audio/music samples/layer1.mp3'],
+    
+    volume:0.2
+  });
+  var sound2 = new Howl({
+    src: ['/audio/music samples/layer2.mp3'],
+    
+    volume:0
+  });
+  var sound3 = new Howl({
+    src: ['/audio/music samples/layer3.mp3'],
+    
+    volume:0
+  });
+  
